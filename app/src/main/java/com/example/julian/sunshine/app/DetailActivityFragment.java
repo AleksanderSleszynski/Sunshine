795f614306b5365957c66720564545787d2669ae
Rasmuhamed <a.sleszynski@gmail.com>
2015-07-24 16:19:54 +0200

    detailActivityFragment getIntent extra data

--- app/src/main/java/com/example/julian/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/julian/sunshine/app/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.julian.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -19,6 +21,12 @@ public class DetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+        }
+
         return rootView;
     }
 }
