fc79feb6a076ac60e98cab7e309eb70111c51cbe
Rasmuhamed <a.sleszynski@gmail.com>
2015-07-24 18:07:39 +0200

    Add Intent to SettingsActivity

--- app/src/main/java/com/example/julian/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/julian/sunshine/app/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.julian.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -36,6 +37,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
