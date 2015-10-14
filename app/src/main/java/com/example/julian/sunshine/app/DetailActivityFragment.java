0c0cd8576921c3166a00a4e2a059d04833743e71
Aleksander <a.sleszynski@gmail.com>
2015-10-14 23:22:50 +0200

    Implement OnItemClickListener in ForecastFragment

--- app/src/main/java/com/example/julian/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/julian/sunshine/app/DetailActivityFragment.java
@@ -36,10 +36,13 @@ public class DetailActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         Intent intent = getActivity().getIntent();
-        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(intent != null){
+            mForecastStr = intent.getDataString();
+        }
+        if(null != mForecastStr) {
             ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
         }
+
         return rootView;
     }
 
--- app/src/main/java/com/example/julian/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/julian/sunshine/app/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.example.julian.sunshine.app;
 
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.julian.sunshine.app.data.WeatherContract;
@@ -102,6 +104,22 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
         final ListView listView = (ListView) rootView.findViewById(R.id.listView_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+                // CursorAdapter returns a cursor at the correct position for getItem(), or null
+                // if it cannot seek to that position.
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                if (cursor != null) {
+                    String locationSetting = Utility.getPreferredLocation(getActivity());
+                    Intent intent = new Intent(getActivity(), DetailActivity.class).
+                            setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(locationSetting, cursor.getLong(COL_WEATHER_DATE)));
+                    startActivity(intent);
+                }
+            }
+        });
 
         return rootView;
     }
